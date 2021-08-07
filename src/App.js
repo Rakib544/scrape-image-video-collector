b38c98ec89b30c785140a7cbe9fb7730c3ffe9f6
Rakib544 <md.rakib10122003@gmail.com>
2021-08-07 10:45:24 +0600

    some commit done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
+import Images from "./components/Images/Images";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/sign-in">
-          <Home />
-        </Route>
-        <Route path="/sign-up">
-          <Home />
-        </Route>
-        <Route path="/images">
-          <Home />
-        </Route>
-        <Route path="/videos">
-          <Home />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/sign-in">
+            <Home />
+          </Route>
+          <Route path="/sign-up">
+            <Home />
+          </Route>
+          <Route path="/images">
+            <Images />
+          </Route>
+          <Route path="/videos">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
